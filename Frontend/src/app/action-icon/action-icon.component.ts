3c8802edb7f260cdb01404693d5361cdbfe2d996
Achref Soltani <60237898+achrefsoltani@users.noreply.github.com>
2022-03-10 11:36:50 +0100

    Basic ActionBar added

--- Frontend/src/app/action-icon/action-icon.component.ts
+++ Frontend/src/app/action-icon/action-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-action-icon',
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ActionIconComponent implements OnInit {
 
   constructor() { }
+
+
+  @Input() image: string;
 
   ngOnInit() {
   }
